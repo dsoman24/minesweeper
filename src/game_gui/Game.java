2cb2fa6e700cac9526e206787e0e2a2bdb4db47d
danieloman1 <dsoman24@gmail.com>
2023-05-19 07:54:34 -0400

    added vscode

--- src/game_gui/Game.java
+++ src/game_gui/Game.java
@@ -73,4 +73,8 @@ public class Game extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
